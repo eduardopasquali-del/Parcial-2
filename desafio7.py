7e36136dfab8a65f02f7bf86cbfed2f8829ada99
eduardopasquali-del <eduardo.pasquali@escola.pr.gov.br>
2026-03-30 08:03:32 -0300

    Add interest calculation based on user input

---
+++ desafio7.py
@@ -0,0 +1,7 @@
+capital = float(input("Capital (C): "))
+taxa = float(input("Taxa (I): "))
+tempo = float(input("Tempo (T): "))
+
+juros = (capital * taxa * tempo) / 100
+
+print("Juros (J):", juros)
